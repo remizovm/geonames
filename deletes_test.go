d04395d8c404b9522198dab4c2fad3706fa6fb96
Remizov Mikhail <00keng@gmail.com>
2016-07-23 23:13:24 +0300

    Added deletes handler.

---
+++ deletes_test.go
@@ -0,0 +1,24 @@
+package geonames
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestDeletes(t *testing.T) {
+	Convey("Test Deletes handler", t, func() {
+		dt := time.Now()
+		year := dt.Year()
+		month := int(dt.Month())
+		day := dt.Day()
+		result, err := Deletes(year, month, day-1)
+		Convey("Error should be nil", func() {
+			So(err, ShouldBeNil)
+		})
+		Convey("Result should not be nil", func() {
+			So(result, ShouldNotBeNil)
+		})
+	})
+}
