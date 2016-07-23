f458e104580620d86d032c5fddc2d7924f1419a3
Remizov Mikhail <00keng@gmail.com>
2016-07-23 23:59:53 +0300

    Added Cities15000 handler and test.

---
+++ cities15000_test.go
@@ -0,0 +1,21 @@
+package geonames
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestCities15000(t *testing.T) {
+	Convey("Test Cities15000 handler", t, func() {
+		result, err := Cities15000()
+		Convey("We should not get an error", func() {
+			So(err, ShouldBeNil)
+		})
+		Convey("Result should be correct", func() {
+			So(result, ShouldNotBeNil)
+			So(result, ShouldNotBeEmpty)
+			So(result[3040051].AsciiName, ShouldEqual, "les Escaldes")
+		})
+	})
+}
